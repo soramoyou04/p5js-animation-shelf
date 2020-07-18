bf4c5cad9d3a5329123aa4c54e98b5083653b18c
Naoki Inomata <soramoyou04@gmail.com>
2020-07-19 08:34:12 +0900

    爆発進捗

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function draw(){
 
   // 花火を打ち上げる間隔を調整
   if (0 === frameCount%45) {
-    fireworks.push(new DrawEllipse());
+    fireworks.push(new FireWork());
   }
 
   for(let i=0; i<fireworks.length; i++){
@@ -31,31 +31,71 @@ function draw(){
     }
 
     // 打ち上げアニメーションを呼び出す
-    if (fireworks[i].getRisingFlg) {
+    if (0 === fireworks[i].getType) {
       fireworks[i].drawRising();
     }
 
     // 打ち上がったら爆発アニメーションを呼び出す
-    // if (array[i].getExplosionFlg) {
-    // }
+    if (0 !== array[i].getType) {
+      fireworks[i].drawExplosion();
+    }
   }
 }
 
-class DrawEllipse {
-  constructor() {
-    // 初期設定 //////////
+class FireWork {
+  // 初期設定
+  constructor(r, g, b, a, vx, vy, gv, type) {
     // 花火の色
-    this.r = random(155) + 100;
-    this.g = random(155) + 100;
-    this.b = random(155) + 100;
-    this.a = 255;
+    if (r) {
+      this.r = r;
+    } else {
+      this.r = random(155) + 100;
+    }
+    if (g) {
+      this.g = g;
+    } else {
+      this.g = random(155) + 100;
+    }
+    if (b) {
+      this.b = b;
+    } else {
+      this.b = random(155) + 100;
+    }
+    if (a) {
+      this.a = a;
+    } else {
+      this.a = 255;
+    }
+
+    // 重力
+    if (vx) {
+      this.vx = vx;
+    }
+    if (vy) {
+      this.vy = vy;
+    }
+    if (gv) {
+      this.gv = gv;
+    }
 
     // 初期位置
-    this.x = random(width);
-    this.y = height;
-
+    if (x) {
+      this.x = x;
+    } else {
+      this.x = random(width);
+    }
+    if (y) {
+      this.y = y;
+    } else {
+      this.y = height;
+    }
+    
     // 花火の玉の大きさ
-    this.w = random(5, 10);
+    if (w) {
+      this.w = w;
+    } else {
+      this.w = random(10, 5);
+    }
 
     // 花火の高さ
     this.maxHeight = random(height/4, height/2);
@@ -69,9 +109,15 @@ class DrawEllipse {
       this.n.push(random(-i/2, i/2));
     }
 
-    // 処理で使うためのステータス
-    this.risingFlg = true;
-    this.explosionFlg = false;
+    // 花火のタイプ
+    if (type) {
+      this.type = type;
+    } else {
+      // 0:打ち上げ
+      this.type = 0;
+    }
+    
+    // フレームカウンター
     this.frame = 0;
   }
 
@@ -83,12 +129,8 @@ class DrawEllipse {
     return this.frame;
   }
 
-  get getRisingFlg() {
-    return this.risingFlg;
-  }
-
-  get getExplosionFlg() {
-    return this.explosionFlg;
+  get getType() {
+    return this.type;
   }
 
   get getDelete() {
@@ -100,7 +142,7 @@ class DrawEllipse {
     this.frame = this.frame + 1;
 
     // 打ち上がるスピード
-    this.y = this.y - (sin((((this.speed*2)-this.frame)/100)*90/this.speed))*((height - this.maxHeight)/100); //  this.y - ((height - this.maxHeight)/(100-(20-this.frame)))
+    this.y = this.y - (sin((((this.speed*2)-this.frame)/100)*90/this.speed))*((height - this.maxHeight)/100);
 
     // 残像を制御
     for (let i=30; 0<i; i--) {
@@ -113,6 +155,18 @@ class DrawEllipse {
     if (this.speed*1.2 < this.frame) {
       this.a = this.a - 5;
     }
+  }
+
+  drawExplosion() {
+    this.frame = this.frame + 1;
+
+    // 爆発のための移動（重力に引っ張られる）
+    this.x += this.vx;
+    this.y += this.vy;
+    this.vy += this.gv;
+
+    update(x, y, w, a);
+    
   }
 
   // 花火を表示する
